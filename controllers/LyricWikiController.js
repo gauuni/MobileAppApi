7adf87d405305323abdd386a4bd0b6ad1805b6d1
Khoi Nguyen <nguyenkhoinguyen082444@gmail.com>
2018-08-20 16:16:12 +0700

    update

--- controllers/LyricWikiController.js
+++ controllers/LyricWikiController.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
-var config = require('../config');
 
 var request    = require('request');
 var cheerio    = require('cheerio');
